953c455832850bba1cb74e21c8a3a4bef3fd9652
denniswoods383 <denniswood383@gmail.com>
2026-04-13 20:54:25 +0000

    fuga

--- src/pages/AdManagementTab.jsx
+++ src/pages/AdManagementTab.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import api from '../api/client';
+
 import {
     DollarSign,
     TrendingUp,
--- src/pages/VideoManagementTab.jsx
+++ src/pages/VideoManagementTab.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
+import api from '../api/client';
+
 import {
     Video,
     Upload,
